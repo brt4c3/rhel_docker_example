d2514f0efde18afcac8a2cf8546182e3d801567d
bro <127320325+brt4c3@users.noreply.github.com>
2025-07-21 15:55:40 +0900

    Update podman-compose.sh

--- jenkins/podman-compose.sh
+++ jenkins/podman-compose.sh
@@ -3,4 +3,6 @@
 export PATH=$PATH:/usr/bin:/usr/local/bin
 export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
+which podman
+podman --version
 exec podman-compose "$@"
